0ebe3872ee1f72eb930accee3c62705098248abf
Mateusz Slomkowski <slomkowskimateusz@gmail.com>
2020-04-15 15:13:34 +0200

    work main.is

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const btn = document.querySelector('button')
 const menuList = document.querySelector('ul');
 
 let i = 1;
+let j = 6;
 
 const addElement = function(){
     const liList = document.createElement('li');
     if (i <= 10) {
         menuList.appendChild(liList);
+        liList.innerHTML += 'element ' + i;
     }
     i++;
+    
+    if (i > 10) {
+       menuList.style.fontSize = j + 'px';
+    }
+    j++;
 }
 
 btn.addEventListener('click', addElement);
